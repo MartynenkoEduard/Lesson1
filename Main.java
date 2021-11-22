4c97e84f22ae9b3bc7934cc9cef1a5fa276b8727
MartynenkoEduard <94779243+MartynenkoEduard@users.noreply.github.com>
2021-11-22 13:02:59 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Exercise_1();
+        Exercise_2();
+    }
+}
+
+
+    private static void Exercise_1() {
+        System.out.println();
+        int x ;
+        int y ;
+        int a = 3 * x / 3 - 7;
+        int b = 4 * x * 3 - 12 * y;
+
+        System.out.println("x =" + x + "y = " + y + "a = 3 * x / 3 - 7 = " + a );
+        System.out.println("x =" + x + "y = " + y + "4 * x * 3 - 12 * y = " + b )
+                // save on GitHub
+    }
+
+xfhgcfjghkhjljkxbndfj545
